e1e4f84f5f23db6e8e814a890bb34bc807495fa1
lionft <91878137+lionft@users.noreply.github.com>
2021-12-19 01:59:10 +0800

    Update Roadmap.js

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -57,9 +57,9 @@ const Roadmap = () => {
                 A plot of Land in the Metaverse for the purpose of organizing
                 virtual art exhibitions will be purchased upon reaching the sale
                 of 33% of Monalisa NFTs (sold out of the First Drop of 3333
-                Monalisa NFTs, which will be released on December 18 th at 9pm
+                Monalisa NFTs, which will be released on December 28 th at 9pm
                 CET, with a Pre-Mint sale only for the Whitelisted members on
-                December 17 th at 9pm CET). <br />
+                December 28 th at 9pm CET). <br />
               </p>
             </div>
             <div
